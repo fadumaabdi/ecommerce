297cc27adef7b3836e1c63d1bd5f8ec44ead236d
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-21 19:30:21 +0100

    category route delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -70,7 +70,23 @@ router.put('/:id', async(req, res) => {
 
 router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
-  
+  try {
+    const categoryData = await Category.destroy(
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+      
+  if (!categoryData) {
+    res.status(404).json({message: 'No category with that ID'});
+    return;
+  } 
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
