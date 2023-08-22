dd647463931705040b3090ce1247f38b27e64b61
faduma abdi <faduma_abdi@hotmail.co.uk>
2023-08-22 15:01:25 +0100

    product tag update with product and tag IDs

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -12,9 +12,21 @@ ProductTag.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    tag_name: {
-      type: DataTypes.STRING,
+    product_id:{
+      type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
     },
   },
   {
